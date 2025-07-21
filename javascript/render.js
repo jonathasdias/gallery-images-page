70dde6bb604606fb4b523d9f0c22cff5bb194991
Jonathas Dias <126801260+jonathasdias@users.noreply.github.com>
2025-07-20 21:21:01 -0300

    render.js

--- javascript/render.js
+++ javascript/render.js
@@ -3,7 +3,7 @@ const slider = document.getElementById('slider');
 
 const imgs = ["https://picsum.photos/600/300?random=1","https://picsum.photos/600/300?random=2",
 "https://picsum.photos/600/300?random=3","https://picsum.photos/600/300?random=4","https://picsum.photos/600/300?random=5",
-"6","https://picsum.photos/600/300?random=7","https://picsum.photos/600/300?random=8",
+"https://picsum.photos/600/300?random=6","https://picsum.photos/600/300?random=7","https://picsum.photos/600/300?random=8",
 "https://picsum.photos/600/300?random=9","https://picsum.photos/600/300?random=10","https://picsum.photos/600/300?random=11",
 "https://picsum.photos/600/300?random=12"]
 
