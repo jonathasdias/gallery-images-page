a736e480711c45f32d970cb672f02cf4b70beecf
Jonathas Dias <126801260+jonathasdias@users.noreply.github.com>
2025-07-20 21:11:38 -0300

    render.js

--- javascript/render.js
+++ javascript/render.js
@@ -1,11 +1,11 @@
 const gallery = document.getElementById('gallery');
 const slider = document.getElementById('slider');
 
-const imgs = ["https://source.unsplash.com/600x300?a=1","https://source.unsplash.com/600x300?b=1",
-"https://source.unsplash.com/600x300?c=1","https://source.unsplash.com/600x300?d=1","https://source.unsplash.com/600x300?e=1",
-"https://source.unsplash.com/600x300?f=1","https://source.unsplash.com/600x300?g=1","https://source.unsplash.com/600x300?h=1",
-"https://source.unsplash.com/600x300?i=1","https://source.unsplash.com/600x300?j=1","https://source.unsplash.com/600x300?k=1",
-"https://source.unsplash.com/600x300?l=1"]
+const imgs = ["https://picsum.photos/600/300?random=1","https://picsum.photos/600/300?random=2",
+"https://picsum.photos/600/300?random=3","https://picsum.photos/600/300?random=4","https://picsum.photos/600/300?random=5",
+"6","https://picsum.photos/600/300?random=7","https://picsum.photos/600/300?random=8",
+"https://picsum.photos/600/300?random=9","https://picsum.photos/600/300?random=10","https://picsum.photos/600/300?random=11",
+"https://picsum.photos/600/300?random=12"]
 
 for(let i = 0; i < imgs.length; i++) {
     const img = `<img id="${"img-" + i}" src="${imgs[i]}" alt="imagem do slider">`
